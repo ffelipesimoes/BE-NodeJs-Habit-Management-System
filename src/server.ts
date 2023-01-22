0085efd58a677f75e1617d412f1385028cb8df4a
Felipe Simões <ffelipesimoes@gmail.com>
2023-01-22 11:07:06 -0300

    feat: created POST habits

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,13 @@
 import fastify from 'fastify';
 import cors from '@fastify/cors'
-import { prisma } from './lib/prisma';
+import { appRoutes } from './routes';
 
 
 const app = fastify()
 
 app.register(cors)
+app.register(appRoutes)
 
-app.get('/', async () => {
-  const habits = await prisma.habit.findMany({
-    
-  })
-  return habits
-})
 
 app.listen({
   port: 3333,
